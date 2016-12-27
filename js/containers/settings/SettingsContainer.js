e7c4c18ef84be6b443fd86ce46652dab62eb3060
Andrea Briozzo <andrea.briozzo@priv.softeco.it>
2016-12-27 19:34:05 +0100

    Connected SettingsContainer

--- js/containers/settings/SettingsContainer.js
+++ js/containers/settings/SettingsContainer.js
@@ -4,6 +4,8 @@ import { bindActionCreators } from 'redux';
 
 import ExtraRootView from './../../presentationals/_base/ExtraRootView';
 import ExtraToolBar  from './../../presentationals/toolBar/ExtraToolBar';
+import * as Actions from './actionCreators';
+import { getMusic, getSound, getVibration, getVolume} from './selectors.js';
 
 export class SettingsContainer extends Component {
 	constructor(props) {
@@ -20,16 +22,35 @@ export class SettingsContainer extends Component {
 }
 
 SettingsContainer.propTypes = {
+	// Settings info
+	music: PropTypes.bool.isRequired,
+	sound: PropTypes.bool.isRequired,
+	vibration: PropTypes.bool.isRequired,
+	volume: PropTypes.number.isRequired,
+	// Navigation Callbacks
 	onBack: PropTypes.func.isRequired,
+	// Actions
+	setSettingsMusic: PropTypes.func.isRequired,
+	setSettingsSound: PropTypes.func.isRequired,
+	setSettingsVibration: PropTypes.func.isRequired,
+	setSettingsVolume: PropTypes.func.isRequired,
 };
 
 const mapStateToProps = state => {
+	const music = getMusic(state);
+	const sound = getSound(state);
+	const vibration = getVibration(state);
+	const volume = getVolume(state);
 	return {
+		music,
+		sound,
+		vibration,
+		volume,
 	};
 };
 
 const mapActionsToProps = dispatch => {
-	return bindActionCreators({}, dispatch);
+	return bindActionCreators(Actions, dispatch);
 };
 
 export default connect(mapStateToProps, mapActionsToProps)(SettingsContainer);
